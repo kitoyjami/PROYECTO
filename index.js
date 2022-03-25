0cf24177125199268a513fda71bef2d1b7fcd15e
kitoyjami <weurbanor@unitru.edu.pe>
2022-03-25 01:16:36 -0500

    cambie la tabla

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var colors =require('colors');
 function sumar(x,y){
     let suma = x+y;
     console.log("La suma es: ".green + suma)
+
 }
 
 sumar(5,8)
@@ -13,5 +14,5 @@ for ( let i=0; i<5 ; i++)
 }
 for ( let j=0; j<5 ; j++)
 {
-    console.log( `Tabla de multiplicar ${j}*2`.blue +" "+ j*2)
+    console.log( `Tabla de multiplicar ${j}*3`.blue +" "+ j*3)
 }
