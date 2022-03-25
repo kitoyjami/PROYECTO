6646a36d7fd4d9d31c07451798f40bc4451d5c31
kitoyjami <weurbanor@unitru.edu.pe>
2022-03-25 00:55:20 -0500

    cambiamos varias cosas

---
+++ index.js
@@ -0,0 +1,17 @@
+var colors =require('colors');
+
+function sumar(x,y){
+    let suma = x+y;
+    console.log("La suma es: ".green + suma)
+}
+
+sumar(5,8)
+
+for ( let i=0; i<5 ; i++)
+{
+    console.log("Hola Mundo".red)
+}
+for ( let j=0; j<5 ; j++)
+{
+    console.log( `Tabla de multiplicar ${j}*2`.blue +" "+ j*2)
+}
